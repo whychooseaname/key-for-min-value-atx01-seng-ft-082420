834d09119860b268c91444c7be42b07b4342f1fa
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-08 07:43:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,15 @@ def key_for_min_value(name_hash)
   name_hash.each do |key,value|
     x=nil 
     y=nil 
-    ifname_hash[key][value]
-    
-    
-    
+    if x == nil
+      x=value
+      y=key
+      elsif x>value
+        x=value
+        y=key 
+      end
+    end
+    y
   end
 
 end
@@ -18,3 +23,5 @@ end
 #if hash [0][1]<x 
 #then x= hash [0][1]
 #return [0]
+
+ 
